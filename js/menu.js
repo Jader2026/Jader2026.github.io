ab9e3ee63a8f8bea99b7bb698f4bf182a7fd30c7
Jader Anaya <86689675+Jader2026@users.noreply.github.com>
2022-06-23 11:40:49 -0500

    Correccion nav --mobil

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,19 @@
 (function(){
-    const openButton = document.querySelector('.nav__menu');
+    const openButton = document.querySelector('.nav__img');
     const menu = document.querySelector('.nav__link');
     const closeMenu = document.querySelector('.nav__close');
+    const controlMenu = document.querySelector('.nav_menu');
 
-    openButton.addEventListener('click', ()=>{
-        menu.classList.add('.nav__link--menu');
+    openButton.addEventListener('click', (e)=>{
+        menu.classList.add('nav__link--show');
+        controlMenu.classList.add('nav_menu--show')
+        console.log(e);
 
     });
 
     closeMenu.addEventListener('click', ()=>{
-        menu.classList.remove('.nav__link--menu');
+        menu.classList.remove('nav__link--show');
+        controlMenu.classList.remove('nav_menu--show')
     }); 
 
 
